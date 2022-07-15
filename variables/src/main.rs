3cae60132f4409e1479c75e5f8b45f313aa5c372
Korakoe <56580073+korakoe@users.noreply.github.com>
2022-07-15 18:57:01 +0800

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+
+    let mut x = 2;
+    x += 3;
+
+    if x == 5 {
+        println!("your number is 5... see! {}", x);
+    }
+
+    else {
+        println!("your number is not 5... see! {}", x);
+    }
+
+}
